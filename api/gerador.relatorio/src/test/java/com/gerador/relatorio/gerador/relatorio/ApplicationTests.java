7555d297bb24da2c80f735dd862efa600f50daf8
pedro <pedro.fischer22@gmail.com>
2022-10-26 22:24:26 -0300

    separando pastas api e front

---
+++ api/gerador.relatorio/src/test/java/com/gerador/relatorio/gerador/relatorio/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerador.relatorio.gerador.relatorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
